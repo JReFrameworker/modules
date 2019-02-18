d75d41837ef6cc549850b246e71aafc21587a357
Benjamin Holland <benjholla@gmail.com>
2019-02-18 10:54:07 -0500

    updating example

--- TestStringReframeworking/src/jref/java/lang/MyString.java
+++ TestStringReframeworking/src/jref/java/lang/MyString.java
@@ -1,20 +1,76 @@
 package jref.java.lang;
 
-import com.jreframeworker.annotations.methods.MergeMethod;
+import com.jreframeworker.annotations.methods.DefineMethod;
 import com.jreframeworker.annotations.types.MergeType;
 
 @MergeType(phase=2)
 public class MyString extends java.lang.String {
 	
+	// This example appears to be not possible to achieve with JReFrameworker (or any bytecode modification tool)
+	// because it violates an underlying assumption in the JVM
+	// https://stackoverflow.com/questions/23471222/trivial-modification-to-java-lang-string-in-rt-jar-causing-vm-segfault
+	
 	private static final long serialVersionUID = 1L;
 	
-	@MergeMethod
+	// original code - STILL BREAKS!
+	@DefineMethod
 	@Override
 	public boolean equals(Object object) {
-		if (object instanceof String) {
-			System.out.println(object);
+		if (this == object) {
+			return true;
 		}
-		return super.equals(object);
+		if ((object instanceof String)) {
+			String str = (String) object;
+			int i = this.value.length;
+			if (i == str.value.length) {
+				char[] arrayOfChar1 = this.value;
+				char[] arrayOfChar2 = str.value;
+				for (int j = 0; i-- != 0; j++) {
+					if (arrayOfChar1[j] != arrayOfChar2[j]) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+		return false;
 	}
+	
+	// replace equals method with merge method contents + original method contents
+//	@DefineMethod
+//	@Override
+//	public boolean equals(Object object) {
+//		if (object instanceof String) {
+//			System.out.println("JREF: " + object);
+//		}
+//		if (this == object) {
+//			return true;
+//		}
+//		if ((object instanceof String)) {
+//			String str = (String) object;
+//			int i = this.value.length;
+//			if (i == str.value.length) {
+//				char[] arrayOfChar1 = this.value;
+//				char[] arrayOfChar2 = str.value;
+//				for (int j = 0; i-- != 0; j++) {
+//					if (arrayOfChar1[j] != arrayOfChar2[j]) {
+//						return false;
+//					}
+//				}
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+	
+	// intercept and print string 
+//	@MergeMethod
+//	@Override
+//	public boolean equals(Object object) {
+//		if (object instanceof String) {
+//			System.out.println("JREF: " + object);
+//		}
+//		return super.equals(object);
+//	}
 
 }
--- TestStringReframeworking/src/jref/java/lang/PreBuild.java
+++ TestStringReframeworking/src/jref/java/lang/PreBuild.java
@@ -1,8 +1,10 @@
 package jref.java.lang;
 
+import com.jreframeworker.annotations.fields.DefineFieldFinality;
+import com.jreframeworker.annotations.fields.DefineFieldVisibility;
 import com.jreframeworker.annotations.types.DefineTypeFinality;
 
 @DefineTypeFinality(phase=1, type="java.lang.String", finality=false)
-public class PreBuild {
-
-}
+@DefineFieldFinality(phase=1, type="java.lang.String", field="value", finality=false)
+@DefineFieldVisibility(phase=1, type="java.lang.String", field="value", visibility="public")
+public class PreBuild {}
